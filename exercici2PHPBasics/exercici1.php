14f8fe201371f364ba270b0d6f6e8983b680b1b5
JuanAndProg <juanandresmunoz85@gmail.com>
2023-05-03 11:13:30 +0200

    exercici1PHP

---
+++ exercici2PHPBasics/exercici1.php
@@ -0,0 +1,10 @@
+<?php
+$tipusInt = 5;
+$tipusDouble = 5.4;
+$tipusString = "exemple";
+$tipusBoolean = true;
+echo "Variable de tipus integer: " . $tipusInt . "<br>";
+echo "Variable de tipus double: " .  $tipusDouble . "<br>";
+echo "Variable de tipus string: " . $tipusString . "<br>";
+echo "Variable de tipus boolean " . $tipusBoolean . "<br>";
+?>
