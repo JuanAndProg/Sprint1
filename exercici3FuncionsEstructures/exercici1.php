aaa17aa95db6d7bca98183d365e3e81f4709dffc
JuanAndProg <juanandresmunoz85@gmail.com>
2023-05-03 11:18:05 +0200

    Funcions1

---
+++ exercici3FuncionsEstructures/exercici1.php
@@ -0,0 +1,10 @@
+<?php
+function parell_o_imparell($num){
+  if($num % 2 == 0){
+    echo "El número $num és parell";
+  } else {
+    echo "El numéro $num és imparell";
+  }
+}
+parell_o_imparell(37); // Exemple d'ús de la funció amb la meva edat.
+?>
