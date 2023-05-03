d6c928a312bfa142629ba11d37f353b950416f2c
JuanAndProg <juanandresmunoz85@gmail.com>
2023-05-03 11:13:55 +0200

    exercici2PHP

---
+++ exercici2PHPBasics/exercici2.php
@@ -0,0 +1,10 @@
+<?php
+
+$hello = "Hello, World!";
+    echo $hello . "<br>";
+    echo strtoupper($hello) . "<br>";
+    echo strlen($hello) . "<br>";
+    echo strrev($hello) . "<br>";
+    $phpCourse = "Aquest és el curs de PHP";
+    echo $hello . " " . $phpCourse;
+?>
